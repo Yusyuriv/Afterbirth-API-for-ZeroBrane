216fca5a9135bb6cca388fde13da959d977bf28f
Yan <Yusyuriv@users.noreply.github.com>
2017-01-07 20:24:16 +0700

    Replaced api+interpreter by a plugin; API update

--- interpreters/bindingofisaacafterbirthplus.lua
+++
@@ -1,9 +0,0 @@
-dofile 'interpreters/luabase.lua'
-local interpreter = MakeLuaInterpreter(5.3, ' Afterbirth+ API')
-interpreter.skipcompile = true
-
-interpreter.name = "Binding of Isaac: Afterbirth+ API"
-interpreter.description = "Binding of Isaac: Afterbirth+ modding API"
-interpreter.api = {"baselib", "afterbirth-plus"}
-
-return interpreter
